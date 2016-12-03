9d3f3109e192b342292d7caba7071710fcb0083f
tsuwandi <timotius.suwandi@gmail.com>
2016-12-03 10:00:15 +0700

    tsi

--- han/src/module/product/bl/ProductBL.java
+++ han/src/module/product/bl/ProductBL.java
@@ -128,7 +128,7 @@ public class ProductBL {
 		Connection con = null;
 		try {
 			con = dataSource.getConnection();
-			return new WoodTypeDAO(con).getWoodType();
+			return new WoodTypeDAO(con).getWoodType1();
 		} finally {
 			con.close();
 		}
