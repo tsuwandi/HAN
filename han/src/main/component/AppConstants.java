462e0ba3a15233d4e0989f8d67d256c175972ce0
tsuwandi <timotius.suwandi@gmail.com>
2017-01-07 03:14:31 +0700

    TSI[01072017] App Constants

---
+++ han/src/main/component/AppConstants.java
@@ -0,0 +1,40 @@
+package main.component;
+
+public class AppConstants {
+
+	public static final int SUPP_TYPE_ID_HASIL_PRODUKSI = 3;
+	public static final String PRODUCTION_TYPE_BARECORE = "Barecore";
+	public static final String PRODUCT_CODE_KLEM_A = "PDC009-3";
+	public static final String PRODUCT_CODE_KLEM_B = "PDC009-4";
+	public static final String PRODUCT_CODE_PROD_RESULT_A = "PDC009-7";
+	public static final String PRODUCT_CODE_PROD_RESULT_B = "PDC009-8";
+	public static final String PRODUCT_CODE_NORMAL_A = "PDC009-1";
+	public static final String PRODUCT_CODE_NORMAL_B = "PDC009-2";
+	
+	public static final String STATUS_COMPLETED = "COMPLETED";
+	public static final String STATUS_FINAL = "FINAL";
+	
+	public static final String BUYING_MODULE = "PEMBELIAN";
+	public static final String RECEIVED = "RECEIVED";
+	public static final String DRY_IN = "DRY_IN";
+	public static final String DRY_OUT = "DRY_OUT";
+	public static final String DEBET = "D";
+	public static final String CREDIT = "C";
+	
+	public static final String BALKEN_BASAH_CD = "B";
+	public static final String BALKEN_KERING_CD = "K";
+	public static final String HASIL_PRODUKSI_CD = "PDC009";
+	public static final String BARANG_PENDUKUNG_CD = "P";
+	
+	public static final String BALKEN_BASAH = "Balken Basah";
+	public static final String BALKEN_KERING = "Balken Kering";
+	public static final String HASIL_PRODUKSI = "Hasil Produksi";
+	public static final String BARANG_PENDUKUNG = "Barang Pendukung";
+	
+	public static final int BALKEN_BASAH_ID = 1;
+	public static final int BALKEN_KERING_ID = 2;
+	public static final int HASIL_PRODUKSI_ID = 3;
+	public static final int BARANG_PENDUKUNG_ID = 4;
+	
+	public static final String CURRENCY_IDR = "IDR";
+}
