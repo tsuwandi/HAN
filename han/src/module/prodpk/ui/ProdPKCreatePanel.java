9ba043a27f034dbfcda858748032d43d5dc04820
tsuwandi <timotius.suwandi@gmail.com>
2017-03-08 16:09:03 +0700

    Update ProdPKCreatePanel.java

--- han/src/module/prodpk/ui/ProdPKCreatePanel.java
+++ han/src/module/prodpk/ui/ProdPKCreatePanel.java
@@ -114,6 +114,8 @@ public class ProdPKCreatePanel extends JPanel implements Bridging {
 
 	JLabel lblBreadcrumb;
 	JLabel lblHeader;
+	private JLabel lblProductionType;
+	private ComboBox<ProductionType> cbProductionType;
 
 	public ProdPKCreatePanel() {
 		prodPK = new ProdPK();
@@ -202,7 +204,10 @@ public class ProdPKCreatePanel extends JPanel implements Bridging {
 				
 				try {
 					listOfLine = new ArrayList<Line>();
-					listOfLine.add(0, new Line(cbGroupShift.getDataIndex().getLineDescription()));
+					Line line = new Line();
+					line.setLineCode(cbGroupShift.getDataIndex().getLineCode());
+					line.setDescription(cbGroupShift.getDataIndex().getLineDescription());
+					listOfLine.add(0, line);
 					cbLine.setList(listOfLine);
 				} catch (Exception e1) {
 					LOGGER.error(e1.getMessage());
@@ -254,6 +259,30 @@ public class ProdPKCreatePanel extends JPanel implements Bridging {
 		lblErrorLine.setBounds(425, 200, 225, 25);
 		panel.add(lblErrorLine);
 		
+		lblProductionType = new JLabel("<html>Tipe Produksi <font color=\"red\">*</font></html>");
+		lblProductionType.setBounds(50, 230, 150, 25);
+		panel.add(lblProductionType);
+		
+		listOfProductionType = new ArrayList<ProductionType>();
+		try {
+			listOfProductionType = ServiceFactory.getProductionPKBL().getAllProductionType();
+			listOfProductionType.add(0, new ProductionType("-- Pilih Tipe Produksi --"));
+		} catch (SQLException e1) {
+			LOGGER.error(e1.getMessage());
+			DialogBox.showErrorException();
+		}
+		cbProductionType = new ComboBox<ProductionType>();
+		cbProductionType.setList(listOfProductionType);
+		cbProductionType.setBounds(220, 230, 150, 25);
+		for(int i = 0; i < listOfProductionType.size(); i++) {
+			if(AppConstants.BC_TYPE_9.equalsIgnoreCase(listOfProductionType.get(i).getProductionTypeCode())) {
+				cbProductionType.setSelectedIndex(i);
+				break;
+			}
+		}
+		cbProductionType.setEnabled(false);
+		
+		panel.add(cbProductionType);
 		
 
 		
