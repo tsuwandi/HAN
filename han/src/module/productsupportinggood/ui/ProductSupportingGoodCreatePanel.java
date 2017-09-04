193d6447d3b68c243716a79945eab43bc6d9f9e8
timotius <timotius@HEIMBL16020.ebworx.com>
2017-09-04 21:16:52 +0700

    Update

--- han/src/module/productsupportinggood/ui/ProductSupportingGoodCreatePanel.java
+++ han/src/module/productsupportinggood/ui/ProductSupportingGoodCreatePanel.java
@@ -3,28 +3,18 @@ package module.productsupportinggood.ui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.text.AbstractDocument;
@@ -32,26 +22,13 @@ import javax.swing.text.DocumentFilter;
 
 import org.apache.log4j.Logger;
 
-import com.toedter.calendar.JDateChooser;
-
-import controller.ServiceFactory;
-import main.component.AppConstants;
 import main.component.ComboBox;
 import main.component.DialogBox;
 import main.component.NumberField;
 import main.component.UppercaseDocumentFilter;
 import main.panel.MainPanel;
-import module.dailyclosing.model.Inventory;
-import module.prodpk.model.ProdPK;
-import module.prodpk.model.ProdPKMaterial;
-import module.prodpk.model.ProdPKResultProduct;
 import module.product.model.ProductCategory;
 import module.product.model.Uom;
-import module.production.model.GroupShift;
-import module.production.model.Line;
-import module.production.model.ProductionType;
-import module.production.model.Shift;
-import module.productionwaste.model.ProductionWaste;
 import module.productsupportinggood.model.ProductSupp;
 import module.supplier.model.Supplier;
 import module.util.Bridging;
@@ -90,6 +67,57 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 	JTextArea txtDescription;
 	//section_description_end
 	
+	//section_attribute_product_start
+	JLabel lblLength;
+	JLabel lblWidth;
+	JLabel lblThickness;
+	JLabel lblUomVolume;
+	JLabel lblWeightNet;
+	JLabel lblWeightGross;
+	JLabel lblUomWeight;
+	NumberField txtLength;
+	NumberField txtWidth;
+	NumberField txtThickness;
+	ComboBox<Uom> cbVolumeUom;
+	NumberField txtWeightNet;
+	NumberField txtWeightGross;
+	ComboBox<Uom> cbWeightUom;
+	//section_attribute_product_end
+	
+	//section_invetory_start
+	JLabel lblFixedAsset;
+	ButtonGroup fixedAsset;
+	JRadioButton fixedAssetYesField;
+	JRadioButton fixedAssetNoField;
+	
+	JLabel lblWarranty;
+	NumberField txtWarranty;
+	JLabel lblWarrantyDay;
+	//section_invetory_end
+	
+	//section_purchase_information_start
+	JLabel lblPurchaseItem;
+	ButtonGroup purchaseItem;
+	JRadioButton purchaseItemYesField;
+	JRadioButton purchaseItemNoField;
+	
+	JLabel lblMininumOrder;
+	NumberField txtMininumOrder;
+	JLabel lblLeadTime;
+	NumberField txtLeadTime;
+	JLabel lblDefaultBuyingCostCenter;
+	NumberField txtDefaultBuyingCostCenter;
+	JLabel lblDefaultBuyingExpenseAccount;
+	NumberField txtDefaultBuyingExpenseAccount;
+	//section_purchase_information_end
+	
+	
+	//section_uom_conversion_start
+	JScrollPane scrollPaneUomConversion;
+	UomConversionTableModel uomConversionTableModel;
+	JTable tblUomConversion;
+	//section_uom_conversion_end
+	
 	JButton btnInsert;
 	JButton btnDelete;
 	JButton btnCancel;
@@ -106,13 +134,16 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 	JLabel lblHeader;
 	JLabel lblHeaderDescription;
 	JLabel lblHeaderAtributeProduct;
+	JLabel lblHeaderInventory;
+	JLabel lblHeaderPurchaseInformation;
+	JLabel lblHeaderConversionUOM;
 	
 	public ProductSupportingGoodCreatePanel() {
 		productSupp = new ProductSupp();
 
 		setLayout(null);
 		panel = new JPanel();
-		panel.setPreferredSize(new Dimension(800, 600));
+		panel.setPreferredSize(new Dimension(800, 1250));
 		panel.setLayout(null);
 
 		lblBreadcrumb = new JLabel("ERP > General > Produk Barang Pendukung");
@@ -126,6 +157,62 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 		panel.add(lblHeader);
 
 		//timotius_section_1_start
+		doAddSectionHeader();
+		//timotius_section_1_end
+		
+		//timotius_section_description_start
+		doAddSectionDescription();
+		//timotius_section_description_end
+		
+		//timotius_section_attribute_product_start
+		doAddSectionAttributeProduct();
+		//timotius_section_attribute_product_end
+		
+		//timotius_section_inventory_start
+		doAddSectionInventory();
+		//timotius_section_inventory_end
+		
+		//timotius_section_purchase_information_start
+		doAddPurchaseInformation();
+		//timotius_section_purchase_information_end
+		
+		doAddConversionUomInformation();
+		
+//		btnSave = new JButton("Simpan");
+//		btnSave.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//
+//			}
+//		});
+//		btnSave.setBounds(925, 570, 100, 25);
+//		panel.add(btnSave);
+//
+//		btnCancel = new JButton("Kembali");
+//		btnCancel.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				 int response = DialogBox.showCloseChoice();
+//				 if (response == JOptionPane.YES_OPTION) {
+//					 MainPanel.changePanel("module.prodpk.ui.ProdPKListPanel");
+//				 }
+//			}
+//		});
+//		btnCancel.setBounds(50, 570, 100, 25);
+//		btnCancel.setFocusable(false);
+//		panel.add(btnCancel);
+		
+		scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setSize(MainPanel.bodyPanel.getSize());
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(32);
+		scrollPane.getHorizontalScrollBar().setUnitIncrement(32);
+		scrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+		add(scrollPane);
+	}
+	
+	public void doAddSectionHeader() {
 		lblProductCode = new JLabel("<html>Kode Produk <font color=\"red\">*</font></html>");
 		lblProductCode.setBounds(50, 80, 150, 25);
 		panel.add(lblProductCode);
@@ -210,9 +297,9 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 		lblImagePath= new JLabel("<html>Gambar</html>");
 		lblImagePath.setBounds(50, 230, 150, 25);
 		panel.add(lblImagePath);
-		//timotius_section_1_end
-		
-		//timotius_section_description_start
+	}
+	
+	public void doAddSectionDescription() {
 		lblHeaderDescription = new JLabel("Deskripsi");
 		lblHeaderDescription.setBounds(50, 265, 200, 25);
 		lblHeaderDescription.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -239,54 +326,203 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 		((AbstractDocument) txtDescription.getDocument()).setDocumentFilter(filter);
 		txtDescription.setEnabled(true);
 		panel.add(txtDescription);
-		//timotius_section_description_end
 		
-		//timotius_section_attribute_product_start
+	}
+	
+	public void doAddSectionAttributeProduct() {
 		lblHeaderAtributeProduct = new JLabel("Atribut Produk");
-		lblHeaderAtributeProduct.setBounds(50, 350, 200, 25);
+		lblHeaderAtributeProduct.setBounds(50, 415, 200, 25);
 		lblHeaderAtributeProduct.setFont(new Font("Tahoma", Font.BOLD, 12));
 		panel.add(lblHeaderAtributeProduct);
-		//timotius_section_attribute_product_end
 		
-		btnSave = new JButton("Simpan");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-//				if (doValidate() == false) {
-//					return;
-//				}
-//				int response = DialogBox.showInsertChoice();
-//				if (response == JOptionPane.YES_OPTION) {
-//					doSave();
-//				}
-			}
-		});
-		btnSave.setBounds(925, 570, 100, 25);
-		panel.add(btnSave);
+		lblLength = new JLabel("<html>Panjang</html>");
+		lblLength.setBounds(50, 445, 150, 25);
+		panel.add(lblLength);
 
-		btnCancel = new JButton("Kembali");
-		btnCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				 int response = DialogBox.showCloseChoice();
-				 if (response == JOptionPane.YES_OPTION) {
-					 MainPanel.changePanel("module.prodpk.ui.ProdPKListPanel");
-				 }
-			}
-		});
-		btnCancel.setBounds(50, 570, 100, 25);
-		btnCancel.setFocusable(false);
-		panel.add(btnCancel);
+		txtLength = new NumberField(4);
+		txtLength.setBounds(220, 445, 150, 25);
+		txtLength.setEnabled(true);
+		panel.add(txtLength);
 		
-		scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setSize(MainPanel.bodyPanel.getSize());
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.getVerticalScrollBar().setUnitIncrement(32);
-		scrollPane.getHorizontalScrollBar().setUnitIncrement(32);
-		scrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		lblWidth = new JLabel("<html>Lebar</html>");
+		lblWidth.setBounds(50, 475, 150, 25);
+		panel.add(lblWidth);
 
-		add(scrollPane);
+		txtWidth = new NumberField(4);
+		txtWidth.setBounds(220, 475, 150, 25);
+		txtWidth.setEnabled(true);
+		panel.add(txtWidth);
+		
+		lblThickness = new JLabel("<html>Tebal</html>");
+		lblThickness.setBounds(50, 505, 150, 25);
+		panel.add(lblThickness);
+
+		txtThickness = new NumberField(4);
+		txtThickness.setBounds(220, 505, 150, 25);
+		txtThickness.setEnabled(true);
+		panel.add(txtThickness);
+		
+		lblUomVolume = new JLabel("<html>Satuan Volume</html>");
+		lblUomVolume.setBounds(400, 445, 150, 25);
+		panel.add(lblUomVolume);
+
+		cbVolumeUom = new ComboBox<Uom>();
+		cbVolumeUom.setList(getAllProductUom());
+		cbVolumeUom.setBounds(480, 445, 150, 25);
+		cbVolumeUom.setEnabled(true);
+		panel.add(cbVolumeUom);
+		
+		lblWeightNet = new JLabel("<html>Berat Netto</html>");
+		lblWeightNet.setBounds(50, 535, 150, 25);
+		panel.add(lblWeightNet);
+
+		txtWeightNet = new NumberField(4);
+		txtWeightNet.setBounds(220, 535, 150, 25);
+		txtWeightNet.setEnabled(true);
+		panel.add(txtWeightNet);
+		
+		lblWeightGross = new JLabel("<html>Berat Gross</html>");
+		lblWeightGross.setBounds(50, 565, 150, 25);
+		panel.add(lblWeightGross);
+
+		txtWeightGross = new NumberField(4);
+		txtWeightGross.setBounds(220, 565, 150, 25);
+		txtWeightGross.setEnabled(true);
+		panel.add(txtWeightGross);
+		
+		lblUomWeight = new JLabel("<html>Satuan Berat</html>");
+		lblUomWeight.setBounds(400, 535, 150, 25);
+		panel.add(lblUomWeight);
+
+		cbWeightUom = new ComboBox<Uom>();
+		cbWeightUom.setList(getAllProductUom());
+		cbWeightUom.setBounds(480, 535, 150, 25);
+		cbWeightUom.setEnabled(true);
+		panel.add(cbWeightUom);
 	}
+	
+	public void doAddSectionInventory() {
+		lblHeaderInventory = new JLabel("Inventori");
+		lblHeaderInventory.setBounds(50, 595, 200, 25);
+		lblHeaderInventory.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panel.add(lblHeaderInventory);
+		
+		lblFixedAsset = new JLabel("<html>Flag Aktiva Tetap</html>");
+		lblFixedAsset.setBounds(50, 625, 150, 25);
+		panel.add(lblFixedAsset);
+
+		fixedAsset = new ButtonGroup();
+		fixedAssetYesField = new JRadioButton("Ya");
+		fixedAssetYesField.setSelected(false);
+		fixedAssetYesField.setBounds(220, 625, 50, 25);
+
+		fixedAssetNoField = new JRadioButton("Tidak");
+		fixedAssetNoField.setSelected(true);
+		fixedAssetNoField.setBounds(290, 625, 50, 25);
+
+		fixedAsset.add(fixedAssetYesField);
+		fixedAsset.add(fixedAssetNoField);
+		
+		panel.add(fixedAssetYesField);
+		panel.add(fixedAssetNoField);
+		
+		lblWarranty = new JLabel("<html>Garansi</html>");
+		lblWarranty.setBounds(50, 655, 150, 25);
+		panel.add(lblWarranty);
+		
+		txtWarranty = new NumberField(3);
+		txtWarranty.setBounds(220, 655, 150, 25);
+		txtWarranty.setEnabled(true);
+		panel.add(txtWarranty);
+		
+		lblWarranty = new JLabel("<html>Hari</html>");
+		lblWarranty.setBounds(400, 655, 150, 25);
+		panel.add(lblWarranty);
+		
+	}
+	
+	public void doAddPurchaseInformation() {
+		
+		lblHeaderPurchaseInformation = new JLabel("Purchase Information");
+		lblHeaderPurchaseInformation.setBounds(50, 685, 200, 25);
+		lblHeaderPurchaseInformation.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panel.add(lblHeaderPurchaseInformation);
+		
+		lblPurchaseItem = new JLabel("<html>Flag Purchase Item</html>");
+		lblPurchaseItem.setBounds(50, 715, 150, 25);
+		panel.add(lblPurchaseItem);
+		
+		purchaseItem = new ButtonGroup();
+		purchaseItemYesField = new JRadioButton("Ya");
+		purchaseItemYesField.setSelected(false);
+		purchaseItemYesField.setBounds(220, 715, 50, 25);
+
+		purchaseItemNoField = new JRadioButton("Tidak");
+		purchaseItemNoField.setSelected(true);
+		purchaseItemNoField.setBounds(290, 715, 50, 25);
+
+		purchaseItem.add(purchaseItemYesField);
+		purchaseItem.add(purchaseItemNoField);
+		
+		panel.add(purchaseItemYesField);
+		panel.add(purchaseItemNoField);
+		
+		lblMininumOrder = new JLabel("<html>Minimum Order</html>");
+		lblMininumOrder.setBounds(50, 745, 150, 25);
+		panel.add(lblMininumOrder);
+		
+		txtMininumOrder = new NumberField(3);
+		txtMininumOrder.setBounds(220, 745, 150, 25);
+		txtMininumOrder.setEnabled(true);
+		panel.add(txtMininumOrder);
+		
+		lblLeadTime = new JLabel("<html>Lead Time</html>");
+		lblLeadTime.setBounds(50, 775, 150, 25);
+		panel.add(lblLeadTime);
+		
+		txtLeadTime = new NumberField(3);
+		txtLeadTime.setBounds(220, 775, 150, 25);
+		txtLeadTime.setEnabled(true);
+		panel.add(txtLeadTime);
+		
+		lblDefaultBuyingCostCenter = new JLabel("<html>Default Buying Cost Center</html>");
+		lblDefaultBuyingCostCenter.setBounds(50, 805, 150, 25);
+		panel.add(lblDefaultBuyingCostCenter);
+		
+		txtDefaultBuyingCostCenter = new NumberField(3);
+		txtDefaultBuyingCostCenter.setBounds(220, 805, 150, 25);
+		txtDefaultBuyingCostCenter.setEnabled(false);
+		panel.add(txtDefaultBuyingCostCenter);
+		
+		lblDefaultBuyingExpenseAccount = new JLabel("<html>Default Expense Account</html>");
+		lblDefaultBuyingExpenseAccount.setBounds(50, 835, 150, 25);
+		panel.add(lblDefaultBuyingExpenseAccount);
+		
+		txtDefaultBuyingExpenseAccount = new NumberField(3);
+		txtDefaultBuyingExpenseAccount.setBounds(220, 835, 150, 25);
+		txtDefaultBuyingExpenseAccount.setEnabled(false);
+		panel.add(txtDefaultBuyingExpenseAccount);
+	}
+	
+	
+	
+	public void doAddConversionUomInformation() {
+		lblHeaderConversionUOM = new JLabel("Konversi Unit Of Measurement");
+		lblHeaderConversionUOM.setBounds(50, 865, 200, 25);
+		lblHeaderConversionUOM.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panel.add(lblHeaderConversionUOM);
+		
+		scrollPaneUomConversion = new JScrollPane();
+		scrollPaneUomConversion.setBounds(50, 905, 975, 150);
+		panel.add(scrollPaneUomConversion);
+		
+		uomConversionTableModel = new UomConversionTableModel(new ArrayList<Uom>());
+		tblUomConversion = new JTable(uomConversionTableModel);
+		tblUomConversion.setFocusable(false);
+		tblUomConversion.setAutoCreateRowSorter(true);
+		scrollPaneUomConversion.setViewportView(tblUomConversion);
+	}
+	
 	
 	public List<ProductCategory> getAllProductCategory() {
 		List<ProductCategory> listOfProductCategory = null;
@@ -316,6 +552,84 @@ public class ProductSupportingGoodCreatePanel extends JPanel implements Bridging
 		return listOfProductUom;
 	}
 	
+	/**
+	 * Class as TableModel for ProductSupportingGood table
+	 * 
+	 * @author TSI
+	 */
+	class UomConversionTableModel extends AbstractTableModel {
+		private static final long serialVersionUID = 1L;
+
+		@SuppressWarnings("rawtypes")
+		private List<Uom> uomConversions;
+
+		public UomConversionTableModel(@SuppressWarnings("rawtypes") List<Uom> uomConversions) {
+			this.uomConversions = uomConversions;
+		}
+
+		/**
+		 * Method to get row count
+		 * 
+		 * @return int
+		 */
+		public int getRowCount() {
+			return uomConversions.size();
+		}
+
+		/**
+		 * Method to get Column Count
+		 */
+		public int getColumnCount() {
+			return 4;
+		}
+
+		/**
+		 * Method to get selected value
+		 * 
+		 * @param rowIndex
+		 *            rowIndex of selected table
+		 * @param columnIndex
+		 *            columnIndex of selected table
+		 * @return ({@link Supplier}) Object
+		 */
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			Object p = uomConversions.get(rowIndex);
+			switch (columnIndex) {
+			case 0:
+				return null;
+			case 1:
+				return null;
+			case 2:
+				return null;
+			case 3:
+				return "<html><u>View</u></html>";
+			default:
+				return "";
+			}
+		}
+
+		/**
+		 * Method to getColumnName
+		 * 
+		 * @param column
+		 *            columnIndex
+		 * @return String column name
+		 */		
+		public String getColumnName(int column) {
+			switch (column) {
+			case 0:
+				return "UOM Acuan";
+			case 1:
+				return "Jumlah";
+			case 2:
+				return "Uom Tujuan";
+			case 3:
+				return "Action";
+			default:
+				return "";
+			}
+		}
+	}
 	
 	@Override
 	public void invokeObjects(Object... objects) {
